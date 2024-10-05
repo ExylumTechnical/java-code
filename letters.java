74012821bbee200ad963725821fa35f684aec83f
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2024-10-05 00:06:21 -0700

    Create letters.java

---
+++ letters.java
@@ -0,0 +1,12 @@
+package letters;
+
+public class Letters {
+	public static void main(String[] args) {
+		char a='a';
+		for(int i =0; i<20; i++) {
+			char shift=(char) (i+a);
+			System.out.println("char shifted to: "+shift);
+			System.out.println('a'+i);
+		}
+	}
+}
